201d978b27b0bef3de9f7e5ec487e11557a3d888
Jamece Coplen <jamececoplen@gmail.com>
2022-08-16 15:42:31 -0600

    implementing heroku db

--- node-postgres/stats_model.js
+++ node-postgres/stats_model.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 const Pool = require('pg').Pool;
-const pool = new Pool({
+const proConfig = {
+  connectionString: process.env.DATABASE_URL
+}
+const devConfig = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT
-});
+}
+const pool = new Pool(proConfig);
 
 const getStats = (body) => {
   return new Promise(function(resolve, reject){
